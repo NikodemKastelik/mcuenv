4c6b8eb214cd780f0cda9651017bd8c6589a06f0
Nikodem Kastelik <nikodemkastelik@gmail.com>
2019-12-21 23:44:01 +0100

    Refactor mocks_generate.sh script #9
    Script should be invoked from directory of repository,
    which needs mock of some other module. Path of directory
    to mock should be passed as argument to the script.

--- test_framework/mocks_generate.sh
+++ test_framework/mocks_generate.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-function mock_given_directory {
-    local dir=$1
-    find $dir -maxdepth 1 -type f | xargs ruby cmock/lib/cmock.rb -ocmock_config.yml
+function abs_path_get {
+    echo "$( cd "$1" ; pwd -P )"
 }
 
-cd "$(dirname "$0")"
-mock_given_directory $1
+invoked_script_path="$(abs_path_get "$(dirname $0)")"
+dir_to_mock_path="$(abs_path_get $1)"
+
+cmock_script=${invoked_script_path}/cmock/lib/cmock.rb
+cmock_config=${invoked_script_path}/cmock_config.yml
+
+find $dir_to_mock_path/*.h -maxdepth 1 -type f | \
+    xargs ruby ${cmock_script} -o${cmock_config}
 
