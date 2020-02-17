de58511405b68f90292b1d24b9e40ec61882e719
Nikodem Kastelik <nikodemkastelik@gmail.com>
2020-02-17 18:47:40 +0100

    Add missing definition of CMAKE_OBJCOPY in boilerplate_app #23
    Definition is needed for post build command
    responsible for .hex file generation.

--- cmake/boilerplate_app.cmake
+++ cmake/boilerplate_app.cmake
@@ -9,6 +9,7 @@ set(CMAKE_SYSTEM_NAME      Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
 set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+set(CMAKE_OBJCOPY    arm-none-eabi-objcopy)
 
 set(CMAKE_EXE_LINKER_FLAGS "--specs=nosys.specs" CACHE INTERNAL "")
 
